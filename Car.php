edc585f358e7b6f0a24143a7da83ab50e755f6b9
Anthony GOUTON <anthony.gouton.wcs@gmail.com>
2022-04-18 20:59:53 +0200

    add files old poo part 3

---
+++ Car.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once 'Vehicle.php';
+
+class Car extends Vehicle
+{
+    public const ALLOWED_ENERGIES = [
+        'fuel',
+        'electric',
+    ];
+
+    private string $energy;
+
+    private int $energyLevel;
+
+    public function __construct(string $color, int $nbSeats, string $energy)
+    {
+        parent::__construct($color, $nbSeats);
+        $this->energy = $energy;
+    }
+
+    public function getEnergy(): string
+    {
+        return $this->energy;
+    }
+
+    public function setEnergy(string $energy): Car
+    {
+        if (in_array($energy, self::ALLOWED_ENERGIES)){
+            $this->energy = $energy;
+        }
+        return $this;
+    }
+
+    public function getEnergyLevel(): int
+    {
+        return $this->energyLevel;
+    }
+
+    public function setEnergyLevel(int $energyLevel): void
+    {
+        $this->energyLevel = $energyLevel;
+    }
+}
